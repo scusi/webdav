8549d83f9856691f449eeb6457a3ec9d32ed5631
Florian Walther <flw@posteo.de>
2018-04-24 21:28:39 +0200

    added a helper command to hash passwords with bcrypt

--- cmd/bcrypt/main.go
+++ cmd/bcrypt/main.go
@@ -1,11 +1,15 @@
+// bcrypt - a helper command to hash a password with bcrypt
+//
+// Usage:
+//
+// bcrypt -p "my super secret passphrase"
+//
 package main
 
 import "fmt"
 import "flag"
 import "log"
 import "golang.org/x/crypto/bcrypt"
-
-//import "encoding/base64"
 
 var passwd string
 
@@ -19,8 +23,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	//str := base64.URLEncoding.EncodeToString(bpass)
 	fmt.Printf("{bcrypt}%s\n", string(bpass))
-
 }
